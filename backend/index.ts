e446a58f56d8db3c2fb104cb845d710551956613
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2023-08-31 18:30:32 +0800

    Fixed Prisma Issues

--- backend/index.ts
+++ backend/index.ts
@@ -4,7 +4,7 @@ import logger from 'morgan';
 import helmet from 'helmet';
 import cors from 'cors';
 import routes from "./routes/routes";
-import sequelize from "./configs/sequelize";
+import prisma from "./configs/prisma";
 dotenv.config();
 
 const app:Express = express();
@@ -16,10 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-sequelize;
-
 app.use("/api", routes);
-app.get('/', (req:Request, res:Response) => { return res.status(200).json({message:""})});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
